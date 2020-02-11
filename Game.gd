fee1df1d37c244a3c125ffff903f39dc6dd1ba27
Neil Macneale <neil.macneale.v@gmail.com>
2020-02-10 19:33:28 -0800

    Can play game, but font is too big and colors are broken

--- Game.gd
+++ Game.gd
@@ -12,24 +12,64 @@ func _ready():
       row.append(null)
     tiles.append(row)
   randomize()
-  var x = randi() % 4
-  var y = randi() % 4
-  create_tile(x, y)
+  #var x = randi() % 4
+  #var y = randi() % 4
+  create_tile(0, 0)
+  tiles[0][0].double()
+  create_tile(0, 1)
+  create_tile(0, 2)
 
 func create_tile(x : int, y : int):
   var tile = get_node("Tile").duplicate()
   print(tile)
-  tile.position.y = y * 268 + 138
-  tile.position.x = x * 268 + 138
+  tile.set_pos(x, y)
   tile.show()
   add_child(tile)
   tiles[y][x] = tile
 
-func update_board(x, y):
-  print("Sliding board " + str(x) + ", " + str(y))
-  x = randi() % 4
-  y = randi() % 4
-  create_tile(x, y)
+func update_board(dir):
+  print("Sliding board " + dir)
+  var x = 0
+  var y = 0
+  for row in 4:
+    var col_items = []
+    for col in 4:
+      var coords = calc_pos(row, col, dir)
+      x = coords[0]
+      y = coords[1]
+      var tile = tiles[y][x]
+      if tile != null:
+        if len(col_items) > 0 and col_items[-1].get_number() == tile.get_number() && col_items[-1].enabled():
+          col_items[-1].double()
+          col_items[-1].disable()
+          tile.queue_free()
+          remove_child(tile)
+        else:
+          col_items.append(tile)
+      tiles[y][x] = null
+    for col in len(col_items):
+      var coords = calc_pos(row, col, dir)
+      x = coords[0]
+      y = coords[1]
+      tiles[y][x] = col_items[col]
+      col_items[col].enable()
+      col_items[col].set_pos(x, y)
+  while true:
+    x = randi() % 4
+    y = randi() % 4
+    if tiles[y][x] == null:
+      create_tile(x, y)
+      break
+
+func calc_pos(row, col, dir):
+  if dir == "up":
+    return [row, col]
+  elif dir == "down":
+    return [row, -col + 3]
+  elif dir == "left":
+    return [col, row]
+  elif dir == "right":
+    return [-col + 3, row]
 
 func _unhandled_input(event):
   if event.is_action_pressed("click"):
@@ -45,22 +85,22 @@ func _calculate_swipe(swipe_end):
     if abs(swipe.y) > minimum_drag:
       if abs(swipe.x) > abs(swipe.y):
         if swipe.x > 0:
-          update_board(1, 0)
+          update_board("right")
         else:
-          update_board(-1, 0)
+          update_board("left")
       else:
         if swipe.y > 0:
-          update_board(0, 1)
+          update_board("down")
         else:
-          update_board(0, -1)
+          update_board("up")
     else:
       if swipe.x > 0:
-        update_board(1, 0)
+        update_board("right")
       else:
-        update_board(-1, 0)
+        update_board("left")
   else:
     if abs(swipe.y) > minimum_drag:
       if swipe.y > 0:
-        update_board(0, 1)
+        update_board("down")
       else:
-        update_board(0, -1)
+        update_board("up")
---
+++ Tile.gd
@@ -0,0 +1,37 @@
+extends Sprite
+
+var number = 2
+var enabled = true
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  pass # Replace with function body.
+
+func get_number():
+  return number
+
+func double():
+  number *= 2
+  print("DOUBLE")
+  get_node("Number").text = str(number)
+
+func set_pos(x, y):
+  self.position.y = y * 268 + 138
+  self.position.x = x * 268 + 138
+
+func enabled():
+  return enabled
+
+func enable():
+  enabled = true
+
+func disable():
+  enabled = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#  pass
